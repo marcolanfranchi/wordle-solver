33a398b497c02f784c16ae084be2ecc1306c0e4f
marcolanfranchi <marcolanfranchi02@gmail.com>
2024-05-23 00:26:15 -0700

    moved words to file

--- wordleSolver.py
+++ wordleSolver.py
@@ -1,30 +1,25 @@
 from itertools import product
-import nltk
-from nltk.corpus import words
 
-# download english words
-nltk.download('words')
+# Function to load words from a file and find possible words
+def findPossibleWords(chars):
+    # Load words from file
+    with open('words.txt', 'r') as file:
+        word_set = set(word.strip().lower() for word in file)
+        word_set = {word for word in word_set if len(word) == 5}
 
-# create word list of len 5 words
-word_list = words.words()
-word_list = [word for word in word_list if len(word) == 5]
-
-# Function to find possible words given a list of characters
-def findPossibleWords(chars=[]):
     if len(chars) != 5:
-            raise ValueError("The input list must contain exactly 5 elements.")
-    else:        
-        # List of all letters
-        letters = 'abcdefghijklmnopqrstuvwxyz'
-        
-        # Generate all possible combinations
-        possible_combinations = product(*[char if char else letters for char in chars])
-        
-        # Check for valid words
-        valid_words = []
-        for combination in possible_combinations:
-            word = ''.join(combination)
-            if word in word_list and len(word) == 5:
-                valid_words.append(word)
-        
-        return valid_words
+        raise ValueError("The input list must contain exactly 5 elements.")
+
+    letters = list('abcdefghijklmnopqrstuvwxyz')
+    
+    # Generate all possible combinations
+    possible_combinations = product(*[char if char else letters for char in chars])
+    
+    # Check for valid words
+    valid_words = []
+    for combination in possible_combinations:
+        word = ''.join(combination)
+        if len(word) == 5 and word in word_set:
+            valid_words.append(word)
+    
+    return valid_words
